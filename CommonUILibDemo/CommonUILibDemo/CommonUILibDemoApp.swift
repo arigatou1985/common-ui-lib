22bcc2f0ca93bc3ed67d8d85ac35603c5bbea9e2
Jing Yu <arigatou1985@gmail.com>
2024-08-26 11:31:40 +0200

    Create a iOS demo app to show case the usage of CommonUILib

---
+++ CommonUILibDemo/CommonUILibDemo/CommonUILibDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommonUILibDemoApp.swift
+//  CommonUILibDemo
+//
+//  Created by Jing Yu on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct CommonUILibDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
